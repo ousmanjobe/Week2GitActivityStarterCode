77c1de8133b0d47725e5d8676ba7fe9dc662220d
calebl37 <lee.cale@northeastern.edu>
2023-09-12 12:37:54 -0400

    implement multiply operation

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import { Calculator } from './calculator';
 const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
-const result_sub = calc.add(3, 2);
+const result_sub = calc.subtract(3, 2);
+const result_mult = calc.multiply(3, 7);
 
 console.log(`result_add = ${result_add}`);
 console.log(`result_subtract = ${result_sub}`);
+console.log(`result_multiply = ${result_mult}`);
 

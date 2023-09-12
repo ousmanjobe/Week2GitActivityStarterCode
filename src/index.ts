427f13b017e9ba92154ad2432ba95d018a9a0c81
calebl37 <lee.cale@northeastern.edu>
2023-09-12 13:02:16 -0400

    fixed typo

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Calculator } from './calculator';
 const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
-const result_sub = calc.add(3, 2);
+const result_sub = calc.subtract(3, 2);
 const result_div = calc.divide(10, 2);
 const result_mult = calc.multiply(3, 7);
 
